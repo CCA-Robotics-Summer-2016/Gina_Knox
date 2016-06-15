e2b1c185358806e3348bb2c8736e760013bed967
ginaknox <gknox@cca.edu>
2016-06-15 13:24:09 -0700

    Add files via upload

---
+++ June_15th_HW.ino
@@ -0,0 +1,49 @@
+const int numberOfReadings = 600;
+int measurements[numberOfReadings];
+const int ldrPin = A0;
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+ 
+void loop() {
+
+  
+ //Read ever 2nd # after 0 
+  for (int evenReadings = 0; evenReadings < numberOfReadings; evenReadings+1) {
+    measurements[evenReadings] = analogRead(ldrPin);
+    delay(1);
+  }
+ 
+ 
+  // calculate the average
+  int sum = 0;
+ 
+  for (int evenReadings = 0; evenReadings < numberOfReadings; evenReadings+1) {
+    sum = sum + measurements[evenReadings];
+  }
+ 
+  int average = sum / numberOfReadings;
+ 
+  // what's the difference between print() and println()?
+  Serial.print("the even readings are ");
+  Serial.println(measurements[highestReading()]);
+}
+ 
+// return the index of the highest reading
+// if there are multiple readings return one of them
+int highestReading() {
+ 
+  int latestHighestReading = 0;
+ 
+  for (int evenReadings = 0; evenReadings < numberOfReadings; evenReadings+1) 
+  { 
+    if (measurements[evenReadings] > measurements[latestHighestReading]) 
+    {
+      latestHighestReading = evenReadings;
+    }
+  }
+  return latestHighestReading;
+ 
+}
